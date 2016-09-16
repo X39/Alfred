456bba80c2d11b0e472efc14080f9b35dfd3dc7e
Marco.Silipo <m.silipo@web.de>
2016-09-16 22:06:26 +0200

    Added roll module

---
+++ modules/roll.lua
@@ -0,0 +1,11 @@
+function testCallback(content, receiver, sender, kind, from, to)
+	local numFrom = tonumber(from);
+	local numTo = tonumber(to);
+	if numFrom == nil or numTo == nil or numFrom > numTo then
+    	alfred.respond(alfred.getRandomResponse("error"), receiver, sender);
+	else
+    	alfred.respond(tostring(math.random(from, to)), receiver, sender);
+	end
+end
+
+alfred.registermsg(testCallback, "roll", "from=0;to=30000;", false, false);
